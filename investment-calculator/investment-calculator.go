0bb4402de81b9c028f36a6a8d788139cfdc42b6f
Thai Duy <chestraxx@gmail.com>
2025-02-28 15:05:10 +0700

    feat: refactor investment calculator with function declaration

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -5,11 +5,12 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	const inflationRate float64 = 2.5
 	var investmentAmount float64
-	var years float64
 	var interestRate float64
+	var years float64
 
 	fmt.Print("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
@@ -20,9 +21,15 @@ func main() {
 	fmt.Print("Enter years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+interestRate/100), years)
-	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+	futureValue, futureRealValue := calcFutureValue(investmentAmount, interestRate, years)
 
 	fmt.Println("Investment amount:", investmentAmount, "Future value:", futureValue)
 	fmt.Println("Investment amount:", investmentAmount, "Future real value:", futureRealValue)
+}
+
+func calcFutureValue(investmentAmount, interestRate, years float64) (futureValue float64, futureRealValue float64) {
+	futureValue = investmentAmount * math.Pow((1+interestRate/100), years)
+	futureRealValue = futureValue / math.Pow((1+inflationRate/100), years)
+
+	return
 }
