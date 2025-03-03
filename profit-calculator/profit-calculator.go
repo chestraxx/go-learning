6e6f4d471bd948d213301d7d5b3e404590b7575f
Thai Duy <chestraxx@gmail.com>
2025-03-03 14:59:16 +0700

    feat: refactor profit calculator app with error handle

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Enter revenue: ")
-	expense := getUserInput("Enter expense: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, errR := getUserInput("Enter revenue: ")
+	expense, errE := getUserInput("Enter expense: ")
+	taxRate, errT := getUserInput("Enter tax rate: ")
+	if errR != nil || errE != nil || errT != nil {
+		fmt.Println(errR, errE, errT)
+		return
+	}
 
 	ebt, profit, ratio := calcFinancial(revenue, expense, taxRate)
 
@@ -14,6 +22,11 @@ func main() {
 	formattedRatio := fmt.Sprintf("Ratio: %.1f \n", ratio)
 
 	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
+	storeResult(ebt, profit, ratio)
+}
+
+func storeResult(ebt, profit, ratio float64) {
+	os.WriteFile("result.txt", []byte(fmt.Sprintf("EBT: %.1f \nProfit: %.1f \nRatio: %.1f \n", ebt, profit, ratio)), 0644)
 }
 
 func calcFinancial(revenue, expense, taxRate float64) (ebt float64, profit float64, ratio float64) {
@@ -23,8 +36,13 @@ func calcFinancial(revenue, expense, taxRate float64) (ebt float64, profit float
 	return
 }
 
-func getUserInput(text string) (value float64) {
+func getUserInput(text string) (value float64, err error) {
 	fmt.Print(text)
 	fmt.Scan(&value)
+
+	if value <= 0 {
+		return 0, errors.New("invalid value")
+	}
+
 	return
 }
