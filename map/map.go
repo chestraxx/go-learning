57c007a1b0f3f5e0e930680f5e4a5072b1456f58
Thai Duy <chestraxx@gmail.com>
2025-03-13 16:21:57 +0700

    feat: work with make func

--- map/map.go
+++ map/map.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// map
 	websites := map[string]string{
 		"Google":   "https://google.com",
 		"Facebook": "https://facebook.com",
@@ -15,4 +16,17 @@ func main() {
 
 	delete(websites, "Google")
 	fmt.Println(websites)
+
+	// make func
+	userNames := make([]string, 2, 5)
+	userNames[0] = "John"
+	userNames[1] = "Jane"
+	fmt.Println(userNames)
+
+	// make func with map
+	courseRatings := make(map[string]float64, 3)
+	courseRatings["Go"] = 4.9
+	courseRatings["Python"] = 4.8
+	courseRatings["JavaScript"] = 4.7
+	fmt.Println(courseRatings)
 }
