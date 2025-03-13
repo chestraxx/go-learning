fa38d8174b755c7eb1b8d9e77069a523fb5fc97d
Thai Duy <chestraxx@gmail.com>
2025-03-13 16:01:48 +0700

    feat: work with map

---
+++ map/map.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":   "https://google.com",
+		"Facebook": "https://facebook.com",
+	}
+
+	fmt.Println(websites)
+}
