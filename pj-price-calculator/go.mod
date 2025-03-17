218de8a575453e15fd85ab738e98facd139f90f6
Thai Duy <chestraxx@gmail.com>
2025-03-17 12:59:52 +0700

    feat: add price calculator app - basic version

---
+++ pj-price-calculator/go.mod
@@ -0,0 +1,3 @@
+module example.com/pj-price-calculator
+
+go 1.22.5
---
+++ pj-price-calculator/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		var priceWithTax []float64 = make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			priceWithTax[priceIndex] = price * (1 + taxRate)
+		}
+
+		result[taxRate] = priceWithTax
+	}
+
+	fmt.Println(result)
+}
