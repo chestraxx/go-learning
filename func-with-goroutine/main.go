799eb8bf2f54c58ec454660798ac3cce67d873d5
Thai Duy <chestraxx@gmail.com>
2025-03-19 22:35:50 +0700

    feat: goroutine with multiple channel

--- func-with-goroutine/main.go
+++ func-with-goroutine/main.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println(phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
@@ -14,14 +15,23 @@ func slowGreet(phrase string, doneChan chan bool) {
 	fmt.Println(phrase)
 
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
+	// dones := make([]chan bool, 4)
 	done := make(chan bool)
-	// go greet("Hello")
-	// go greet("Hi")
-	go slowGreet("Hello", done)
-	// go greet("Hi")
 
-	<-done
+	// dones[0] = make(chan bool)
+	go greet("Hello1", done)
+	// dones[1] = make(chan bool)
+	go greet("Hi1", done)
+	// dones[2] = make(chan bool)
+	go slowGreet("Hello 2 ", done)
+	// dones[3] = make(chan bool)
+	go greet("Hi 2 ", done)
+
+	for range done {
+		// fmt.Println(doneChan)
+	}
 }
