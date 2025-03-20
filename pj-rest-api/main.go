5f56ed10b2eddf2fe8899572c8f01527b7bca234
Thai Duy <chestraxx@gmail.com>
2025-03-20 17:36:59 +0700

    feat: rest-api app add event create api

--- pj-rest-api/main.go
+++ pj-rest-api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"example.com/pj-rest-api/model"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/event", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"data": "Hello World"})
+	events := model.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event model.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	fmt.Println(event)
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
