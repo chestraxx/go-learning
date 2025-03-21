977bc4d45c914964ffa2ea8cfb3254be80768b5a
Thai Duy <chestraxx@gmail.com>
2025-03-21 15:42:02 +0700

    feat: rest-api app implement hash to password at signup user api

--- pj-rest-api/model/user.go
+++ pj-rest-api/model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "example.com/pj-rest-api/db"
+import (
+	"example.com/pj-rest-api/db"
+	"example.com/pj-rest-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -19,7 +22,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
--- pj-rest-api/route/route.go
+++ pj-rest-api/route/route.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func InitRoute(r *gin.Engine) {
 	r.GET("/events", getEvents)
 	r.GET("/events/:id", getEvent)
-	r.POST("/events", createEvent)
+	r.POST("/event", createEvent)
 	r.PUT("/event/:id", updateEvent)
 	r.DELETE("/event/:id", deleteEvent)
 	r.POST("/signup", signup)
