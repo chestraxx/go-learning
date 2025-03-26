84c8560303c445085250d3de1435118c55fdb957
Thai Duy <chestraxx@gmail.com>
2025-03-26 16:27:15 +0700

    feat: rest-api app add unregister event api

--- pj-rest-api/route/register.go
+++ pj-rest-api/route/register.go
@@ -32,5 +32,24 @@ func registerForEvent(context *gin.Context) {
 }
 
 func unregisterForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 
+	event, err := model.GetEvent(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	err = event.Unregister(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Unregistered for event successfully"})
 }
